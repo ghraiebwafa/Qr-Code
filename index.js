41f8f0a34fb308da14c3a6307f6392d5cd5de8aa
wafa ghraieb <wafaaghraieb@gmail.com>
2024-03-20 11:15:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fr from 'fs';
+
+inquirer
+  .prompt([
+    {message:"type in your URL",
+    name:"URL"}
+  ])
+  .then((answers) => {
+   const url=answers.url;
+   var qr_svg = qr.image(url);
+  qr_svg.pipe(fs.createWriteStream('iqr2_image.png'));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+
+
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
